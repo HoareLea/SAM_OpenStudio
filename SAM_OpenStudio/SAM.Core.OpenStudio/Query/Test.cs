cb1c195f520fd72fa38ab01b90f3e067c29b3f39
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-01-28 10:02:21 +0000

    Initial Implementation

---
+++ SAM_OpenStudio/SAM.Core.OpenStudio/Query/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SAM.Core.OpenStudio
+{
+    public static partial class Query
+    {
+        public static void Test(string path)
+        {
+            global::OpenStudio.Path openStudioPath = global::OpenStudio.OpenStudioUtilitiesCore.toPath(path);
+
+            global::OpenStudio.SqlFile sqlFile = new global::OpenStudio.SqlFile(openStudioPath);
+
+            global::OpenStudio.OptionalDouble optionalDouble = sqlFile.electricityPumps();
+            double @double = optionalDouble.get();
+        }
+    }
+}
