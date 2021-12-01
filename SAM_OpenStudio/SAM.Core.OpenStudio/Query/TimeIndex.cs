6ca46cbf65222b7f3db5a558644edc87c0e42c85
Michal Dengusiak <tassimulation@gmail.com>
2021-12-01 22:51:58 +0000

    WIP

--- SAM_OpenStudio/SAM.Core.OpenStudio/Query/TimeIndex.cs
+++ SAM_OpenStudio/SAM.Core.OpenStudio/Query/TimeIndex.cs
@@ -1,18 +1,20 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace SAM.Core.OpenStudio
 {
     public static partial class Query
     {
-        public static int TimeIndex(this DataTable dataTable, ShortDateTime peakDate, int environemntPeriodIndex)
+        public static int TimeIndex(this DataTable dataTable, ShortDateTime peakDate, int environemntPeriodIndex, bool exactMatch = false)
         {
-            if(dataTable == null || peakDate == null || environemntPeriodIndex == -1)
+            if (dataTable == null || peakDate == null || environemntPeriodIndex == -1)
             {
                 return -1;
             }
 
             DataColumnCollection dataColumnCollection = dataTable.Columns;
-            if(dataColumnCollection == null)
+            if (dataColumnCollection == null)
             {
                 return -1;
             }
@@ -24,7 +26,7 @@ namespace SAM.Core.OpenStudio
             }
 
             int index_TimeIndex = dataColumnCollection.IndexOf("TimeIndex");
-            if(index_TimeIndex == -1)
+            if (index_TimeIndex == -1)
             {
                 return -1;
             }
@@ -57,12 +59,14 @@ namespace SAM.Core.OpenStudio
             int index_Second = dataColumnCollection.IndexOf("Dst");
 
             DataRowCollection dataRowCollection = dataTable.Rows;
-            if(dataRowCollection == null)
+            if (dataRowCollection == null)
             {
                 return -1;
             }
 
-            foreach(DataRow dataRow in dataRowCollection)
+            List<Tuple<int, int>> tuples_Minute = new List<Tuple<int, int>>();
+            List<Tuple<int, int>> tuples_Second = new List<Tuple<int, int>>();
+            foreach (DataRow dataRow in dataRowCollection)
             {
                 if (!Core.Query.TryConvert(dataRow[index_EnvironmentPeriodIndex], out int environemntPeriodIndex_Temp))
                 {
@@ -79,7 +83,7 @@ namespace SAM.Core.OpenStudio
                     continue;
                 }
 
-                if(year_Temp != 0)
+                if (year_Temp != 0)
                 {
                     continue;
                 }
@@ -99,22 +103,61 @@ namespace SAM.Core.OpenStudio
                     continue;
                 }
 
-                if (index_Minute != -1 && (!Core.Query.TryConvert(dataRow[index_Minute], out byte minute_Temp) || !peakDate.Minute.Equals(minute_Temp)))
-                {
-                    continue;
-                }
-
-                if (index_Second != -1 && (!Core.Query.TryConvert(dataRow[index_Second], out byte second_Temp) || !peakDate.Second.Equals(second_Temp)))
-                {
-                    continue;
-                }
-
                 if (!Core.Query.TryConvert(dataRow[index_TimeIndex], out int result))
                 {
                     continue;
                 }
 
+                if (index_Minute != -1)
+                {
+                    if(!Core.Query.TryConvert(dataRow[index_Minute], out byte minute_Temp))
+                    {
+                        continue;
+                    }
+
+                    if(!peakDate.Minute.Equals(minute_Temp))
+                    {
+                        if (!exactMatch)
+                        {
+                            tuples_Minute.Add(new Tuple<int, int>(result, minute_Temp));
+                        }
+
+                        continue;
+                    }
+                }
+
+                if (index_Second != -1)
+                {
+                    if(!Core.Query.TryConvert(dataRow[index_Second], out byte second_Temp))
+                    {
+                        continue;
+                    }
+
+                    if(!peakDate.Second.Equals(second_Temp))
+                    {
+                        if (!exactMatch)
+                        {
+                            tuples_Second.Add(new Tuple<int, int>(result, second_Temp));
+                        }
+
+                        continue;
+                    }
+                }
+
                 return result;
+            }
+
+            if(tuples_Second != null && tuples_Second.Count != 0)
+            {
+                tuples_Second.Sort((x, y) => Math.Abs(x.Item2 - peakDate.Second).CompareTo(Math.Abs(x.Item2 - peakDate.Second)));
+                return tuples_Second[0].Item1;
+            }
+
+
+            if (tuples_Minute != null && tuples_Minute.Count != 0)
+            {
+                tuples_Minute.Sort((x, y) => Math.Abs(x.Item2 - peakDate.Minute).CompareTo(Math.Abs(x.Item2 - peakDate.Minute)));
+                return tuples_Minute[0].Item1;
             }
 
             return -1;
